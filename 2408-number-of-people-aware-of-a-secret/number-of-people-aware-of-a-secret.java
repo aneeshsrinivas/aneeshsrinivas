efe5bbe970fc679ab2bce18c6c783e451debdd8e
Aneesh Srinivas <aneeshsrinivas2006@gmail.com>
2025-09-09 18:56:02 +0530

    Time: 2 ms (100.00%) | Memory: 40.5 MB (94.55%) - LeetSync

---
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        int mod = 1000000007;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int share = 0;
+        
+        for (int day = 2; day <= n; day++) {
+            if (day - delay >= 1) {
+                share = (share + dp[day - delay]) % mod;
+            }
+            if (day - forget >= 1) {
+                share = (share - dp[day - forget] + mod) % mod;
+            }
+            dp[day] = share;
+        }
+        
+        int result = 0;
+        for (int day = n - forget + 1; day <= n; day++) {
+            result = (result + dp[day]) % mod;
+        }
+        
+        return result;
+    }
+}
